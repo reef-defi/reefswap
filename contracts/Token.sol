1762aeb3f65ab49c37af7e8977c64f421c19b1ab
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-03-15 22:44:46 +0100

    Two new tokens for trading bot

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,3 +8,14 @@ contract Token is ERC20 {
 				_mint(msg.sender, initialBalance);
 		}
 }
+contract LotrToken is ERC20 {
+		constructor(uint256 initialBalance) public ERC20("lord of the rings", "LOTR") {
+				_mint(msg.sender, initialBalance);
+		}
+}
+
+contract SwToken is ERC20 {
+		constructor(uint256 initialBalance) public ERC20("Swar wars", "SW") {
+				_mint(msg.sender, initialBalance);
+		}
+}
